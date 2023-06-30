33bddba4693f8b928b2d797e51d3fc8c4240066a
marhano <carullomarjan@gmail.com>
2023-06-30 16:07:02 +0800

    .added docs

--- ngx/index.d.ts
+++ ngx/index.d.ts
@@ -22,24 +22,72 @@ import { AwesomeCordovaNativePlugin } from '@awesome-cordova-plugins/core';
  */
 export declare class FaceMe extends AwesomeCordovaNativePlugin {
     /**
-     *
-     * @return {Promise<any>} Returns a promise that resolves when something happens
-     */
+       * This function is used for testing the Face Me plugin.
+       * @return {Promise<any>} Returns a promise that resolves when the test completes.
+       */
     testPlugin(): Promise<any>;
     /**
-     *
-     * @returns
-     */
+      * Initializes the Face Me SDK.
+      * @returns {Promise<any>} Returns a promise that resolves when the SDK is initialized.
+      */
     initializeSDK(): Promise<any>;
+    /**
+      * Activates the Face Me license.
+      * @returns {Promise<any>} Returns a promise that resolves when the license is activated.
+      */
     activateLicense(): Promise<any>;
+    /**
+      * Deactivates the Face Me license.
+      * @returns {Promise<any>} Returns a promise that resolves when the license is deactivated.
+      */
     deactivateLicense(): Promise<any>;
+    /**
+      * Detects a face from a base64 encoded image.
+      * @param {string} base64Image The base64 encoded image.
+      * @returns {Promise<any>} Returns a promise that resolves with the face detection result.
+      */
     detectFace(base64Image: string): Promise<any>;
-    enrollFace(): Promise<any>;
+    /**
+      * Enrolls a face from a base64 encoded image.
+      * @param {string} base64Image The base64 encoded image.
+      * @returns {Promise<any>} Returns a promise that resolves when the face enrollment is completed.
+      */
+    enrollFace(base64Image: string): Promise<any>;
+    /**
+      * Recognizes a face.
+      * @returns {Promise<any>} Returns a promise that resolves with the face recognition result.
+      */
     recognizeFace(): Promise<any>;
+    /**
+      * Deletes a face with the specified face ID.
+      * @param {number} faceId The ID of the face to delete.
+      * @returns {Promise<any>} Returns a promise that resolves when the face is deleted.
+      */
     deleteFace(faceId: number): Promise<any>;
+    /**
+      * Updates a face.
+      * @returns {Promise<any>} Returns a promise that resolves when the face update is completed.
+      */
     updateFace(): Promise<any>;
+    /**
+      * Selects a face.
+      * @returns {Promise<any>} Returns a promise that resolves when the face selection is completed.
+      */
     selectFace(): Promise<any>;
+    /**
+      * Adds a face with the specified username.
+      * @param {string} username The username associated with the face.
+      * @returns {Promise<any>} Returns a promise that resolves when the face is added.
+      */
     addFace(username: string): Promise<any>;
+    /**
+      * Starts the anti-spoofing process.
+      * @returns {Promise<any>} Returns a promise that resolves when the anti-spoofing process is started.
+      */
     startAntiSpoofing(): Promise<any>;
+    /**
+      * Stops the anti-spoofing process.
+      * @returns {Promise<any>} Returns a promise that resolves when the anti-spoofing process is stopped.
+      */
     stopAntiSpoofing(): Promise<any>;
 }
